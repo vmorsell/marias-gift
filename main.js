0577d4d7ad1d4b9744e3bec1a88c1a3f150c2c7c
Viktor <viktor@intrykk.se>
2020-02-12 11:23:43 +0100

    Baloons are fun. Pop one, get 20 more

--- main.js
+++ main.js
@@ -49,7 +49,7 @@ const app = () => {
 
   const handleBalloonClick = $balloon => {
     popBalloon($balloon);
-    dispatchBalloons(1);
+    dispatchBalloons(20);
   };
 
   $box.onclick = handleBoxClick;
